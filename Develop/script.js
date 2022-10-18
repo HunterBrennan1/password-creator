c5a9da844106a9963f32f8ee615c3ace56a8b98e
Hunter <hunterbrennan15@gmail.com>
2022-10-18 19:15:08 -0400

    added a console log of random numbers when the (generate passwaord) button is pressed

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,19 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+function randomInt(min, max){
+  if (!max){
+    max = min
+    min = 0
+  }
+  var rand = Math.random()
+  return Math.floor(min + (1 - rand) + rand*max)
+}
+
+function getRandomItem(list){
+  return list[randomInt(0, list.length - 1)]
+}
+
 function generatePassword(){
 
     var userInput = window.prompt("How long is your password?")
@@ -17,8 +30,46 @@ function generatePassword(){
     }
 
     var userNumbers = window.confirm("would you like to include numbers in your password?")
+    var userSymbols = window.confirm("would you like to include symbols in your password?")
+    var userLowercase = window.confirm("would you like to include Lowercase letters in your password?")
+    var userUppercase = window.confirm("would you like to include Uppercase letters in your password?")
 
-  return "Generated password"
+    var numberList = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+    var symbolList = ["!", "@", "#", "$", "%", "^", "&", "*"]
+    var lowercaseList = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+    var uppercaseList = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+
+    var optionsList = []
+
+    for (var i = 0; i < lowercaseList.length; i++){
+      uppercaseList[i] = lowercaseList[i].toUpperCase()
+    }
+
+    if (userNumbers === true){
+      optionsList.push(numberList)
+    }
+
+    if (userSymbols === true){
+      optionsList.push(symbolList)
+    }
+
+    if (userLowercase === true){
+      optionsList.push(lowercaseList)
+    }
+
+    if (userUppercase === true){
+      optionsList.push(uppercaseList)
+    }
+
+    var generatedPassword = ""
+
+    for (var i = 0; i < passwordLength; i++){
+      var randomList = getRandomItem(optionsList)
+      var randomChar = getRandomItem(randomList)
+      console.log(randomChar)
+    }
+
+  
 }
 
 
